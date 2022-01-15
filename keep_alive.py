190b6e5918f5babe46e639ef18c591e192434447
Youngshangsaucy <75769334+Shangmin-Chen@users.noreply.github.com>
2022-01-15 02:45:37 +0000

    update style and function, increased selection to check for false inputs.

--- keep_alive.py
+++ keep_alive.py
@@ -14,49 +14,57 @@ def my_form_post():
     fname = request.form["First"]
     lname = request.form["Last"]
     email = request.form["Mail"]
-    yn = request.form["yesno"]
+    create = request.form.get("Create")
+    delete = request.form.get("Delete")
+    if create == "create":
+      create = True
+    if delete == "delete":
+      delete = True
+    if "" == fname or "" == lname or "" == email:
+      return render_template("emptyerror.html")
+    
+    elif create == delete:
+      return render_template("one_only.html")
+    
+    elif "@" not in email:
+      return render_template("mailerror.html")
 
-    if yn == "Delete":
+    elif delete == True:
       database = db["database"]
       count = 0
       for i in database:
         count += 1
         for j in i:
-          if email in j:
+          if email.upper() in j.upper():
             del database[count-1]
             db["database"] = database
             data = (fname.capitalize(), lname.capitalize())
             return render_template("successdel.html", data=data)
 
-    elif "" == fname or "" == lname or "" == email or "" == yn:
-      return render_template("emptyerror.html")
-    
-    elif "@" not in email:
-      return render_template("mailerror.html")
-
     else:
       database = db["database"]
       for i in database:
         for j in i:
-          if email in j:
+          if email.upper() in j.upper():
             data = email
             return render_template("alreadyexist.html", data=data)
+   
+    if create == True:
+      print("New user registered: {} {} {}".format(fname, lname, email))
 
-    print("New user registered: {} {} {}".format(fname, lname, email))
-
-    if "database" in db.keys():
-      database = db["database"]
-      data = [fname, lname, email]
-      database.append(data)
-      db["database"] = database        
-      data = (fname.capitalize(), lname.capitalize())
-      return render_template("successadd.html", data=data)
-    else:
-      database = []
-      data = [[fname, lname, email]]
-      db["database"] = data
-      data = (fname.capitalize(), lname.capitalize())
-      return render_template("successadd.html", data=data)
+      if "database" in db.keys():
+        database = db["database"]
+        data = [fname, lname, email]
+        database.append(data)
+        db["database"] = database        
+        data = (fname.capitalize(), lname.capitalize())
+        return render_template("successadd.html", data=data)
+      else:
+        database = []
+        data = [[fname, lname, email]]
+        db["database"] = data
+        data = (fname.capitalize(), lname.capitalize())
+        return render_template("successadd.html", data=data)
 
 def run():
   app.run(host='0.0.0.0', port=8080)
